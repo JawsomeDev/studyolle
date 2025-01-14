b2628da10775eeeba9e40edeac55761cf49433e1
hyuk2 <hyuk2000s@gmail.com>
2025-01-14 15:41:03 +0900

    스터디 개설

--- src/main/java/com/studyolle/account/validator/SignUpFormValidator.java
+++ src/main/java/com/studyolle/account/validator/SignUpFormValidator.java
@@ -27,7 +27,7 @@ public class SignUpFormValidator implements Validator {
             errors.rejectValue("email", "invalid.email", new Object[]{signUpForm.getEmail()}, "이미 사용중인 이메일 입니다.");
         }
         if(accountRepository.existsByNickname(signUpForm.getNickname())){
-            errors.rejectValue("email", "invalid.nickname", new Object[]{signUpForm.getNickname()}, "이미 사용중인 닉네임 입니다.");
+            errors.rejectValue("nickname", "invalid.nickname", new Object[]{signUpForm.getNickname()}, "이미 사용중인 닉네임 입니다.");
         }
     }
 }
